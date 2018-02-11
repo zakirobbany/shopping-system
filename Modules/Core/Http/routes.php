5cecff5a556e193583b54713251361793f32f754
zaki <zaki.robbany22@gmail.com>
2018-02-11 07:43:14 +0700

    added core routes

--- Modules/Core/Http/routes.php
+++ Modules/Core/Http/routes.php
@@ -3,4 +3,9 @@
 Route::group(['middleware' => 'web', 'prefix' => 'core', 'namespace' => 'Modules\Core\Http\Controllers'], function()
 {
     Route::get('/', 'CoreController@index');
+    Route::resource('/company-profile', 'CompanyProfileController');
+    Route::resource('/customer', 'CustomerController');
+    Route::resource('/employee', 'EmployeeController');
+    Route::resource('/supplier', 'SupplierController');
+    Route::resource('/courier', 'CourierController');
 });
