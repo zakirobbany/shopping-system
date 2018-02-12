5cde43a20cbd93693fbb03c8370615b6f86f7e43
zaki <zaki.robbany22@gmail.com>
2018-02-12 11:05:49 +0700

    Added Delete and Update Routes for Courier and Employee Controller

--- Modules/Core/Http/routes.php
+++ Modules/Core/Http/routes.php
@@ -5,8 +5,22 @@ Route::group(['middleware' => 'web', 'prefix' => 'core', 'namespace' => 'Modules
     Route::get('/', 'CoreController@index');
     Route::resource('/company-profile', 'CompanyProfileController');
     Route::resource('/customer', 'CustomerController');
+
+    /*
+     * Employee Routes
+     * */
     Route::resource('/employee', 'EmployeeController');
+    Route::post('/employee/{employee}/update', 'EmployeeController@update')->name('employee.update');
+    Route::get('/employee/{employee}/delete', 'EmployeeController@delete')->name('employee.delete');
+
     Route::resource('/supplier', 'SupplierController');
+
+    /*
+     * Courier Routes
+     * */
     Route::resource('/courier', 'CourierController');
+    Route::post('/courier/{courier}/update', 'CourierController@update')->name('courier.update');
+    Route::get('/courier/{courier}/delete', 'CourierController@delete')->name('courier.delete');
+
     Route::resource('/courier-type', 'CourierTypeController');
 });
