cef167de817c8c5e6f389984fd7e2c99ba8c3db6
zaki <zaki.robbany22@gmail.com>
2018-02-15 13:12:29 +0700

    Added Supplier Request

---
+++ Modules/Core/Http/Requests/SupplierRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Core\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SupplierRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->method()) {
+            case 'PUT':
+                return [
+                    'name' => 'required',
+                    'address' => 'required',
+                    'phone_number' => 'required',
+                ];
+                break;
+            case 'POST':
+                return [
+                    'name' => 'required',
+                    'address' => 'required',
+                    'phone_number' => 'required',
+                ];
+                break;
+            default :
+                return [];
+                break;
+        }
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
