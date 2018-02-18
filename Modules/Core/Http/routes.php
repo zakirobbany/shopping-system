302ea0de869acbf6473a1efc50847a117d688656
zaki <zaki.robbany22@gmail.com>
2018-02-18 09:34:53 +0700

    Update routes : # added courier-type routes # added customer routes

--- Modules/Core/Http/routes.php
+++ Modules/Core/Http/routes.php
@@ -3,8 +3,19 @@
 Route::group(['middleware' => 'web', 'prefix' => 'core', 'namespace' => 'Modules\Core\Http\Controllers'], function()
 {
     Route::get('/', 'CoreController@index');
+
+    /*
+     * Company Profile Routes
+     * */
     Route::resource('/company-profile', 'CompanyProfileController');
+    Route::post('/company-profile/{companyProfile}/update', 'CompanyProfileController@update')->name('company-profile.update');
+
+    /*
+     * Customer Routes
+     * */
     Route::resource('/customer', 'CustomerController');
+    Route::post('/customer/{customer}/update', 'CustomerController@update')->name('customer.update');
+    Route::get('/customer/{customer}/delete', 'CustomerController@destroy')->name('customer.delete');
 
     /*
      * Employee Routes
@@ -13,6 +24,9 @@ Route::group(['middleware' => 'web', 'prefix' => 'core', 'namespace' => 'Modules
     Route::post('/employee/{employee}/update', 'EmployeeController@update')->name('employee.update');
     Route::get('/employee/{employee}/delete', 'EmployeeController@delete')->name('employee.delete');
 
+    /*
+     * Supplier Controller
+     * */
     Route::resource('/supplier', 'SupplierController');
     Route::post('/supplier/{supplier}/update', 'SupplierController@update')->name('supplier.update');
     Route::get('/supplier/{supplier}/delete', 'SupplierController@destroy')->name('supplier.delete');
@@ -24,5 +38,10 @@ Route::group(['middleware' => 'web', 'prefix' => 'core', 'namespace' => 'Modules
     Route::post('/courier/{courier}/update', 'CourierController@update')->name('courier.update');
     Route::get('/courier/{courier}/delete', 'CourierController@delete')->name('courier.delete');
 
+    /*
+     * CourierType Routes
+     * */
     Route::resource('/courier-type', 'CourierTypeController');
+    Route::post('/courier-type/{courierType}/update', 'CourierTypeController@update')->name('courier-type.update');
+    Route::get('/courier-type/{courierType}/delete', 'CourierTypeController@destroy')->name('courier-type.delete');
 });
