4a8f56b5645002c641d81cc527c97691ae08a467
zaki <zaki.robbany22@gmail.com>
2018-03-04 08:13:07 +0700

    Update route : - added add stock and store add stock route

--- Modules/Inventory/Http/routes.php
+++ Modules/Inventory/Http/routes.php
@@ -5,21 +5,23 @@ Route::group(['middleware' => 'web', 'prefix' => 'inventory', 'namespace' => 'Mo
     Route::get('/', 'InventoryController@index');
 
     /*
-     * Product Controller
+     * Product Routes
      * */
-    Route::resource('/product', 'ProductController');
+    Route::resource('/product', 'ProductController')->except(['show']);
     Route::post('/product/{product}/update', 'ProductController@update')->name('product.update');
     Route::get('/product/{product}/delete', 'ProductController@destroy')->name('product.delete');
+    Route::get('/product/add-stock', 'ProductController@addStock')->name('product.add-stock');
+    Route::post('/product/add-stock/store', 'ProductController@storeAddStock')->name('product.store-add-stock');
 
     /*
-     * Product Controller
+     * Product Type Routes
      * */
     Route::resource('/product-type', 'ProductTypeController');
     Route::post('/product-type/{productType}/update', 'ProductTypeController@update')->name('product-type.update');
     Route::get('/product-type/{productType}/delete', 'ProductTypeController@destroy')->name('product-type.delete');
 
     /*
-     * Product Controller
+     * Product Brand Routes
      * */
     Route::resource('/product-brand', 'ProductBrandController');
     Route::post('/product-brand/{productBrand}/update', 'ProductBrandController@update')->name('product-brand.update');
