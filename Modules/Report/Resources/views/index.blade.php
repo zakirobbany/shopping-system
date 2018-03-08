baa4aabf7fb46a6c9e582284019c068ca3f8406f
zaki <zaki.robbany22@gmail.com>
2018-03-08 16:13:44 +0700

    Update report lobby

--- Modules/Report/Resources/views/index.blade.php
+++ Modules/Report/Resources/views/index.blade.php
@@ -1,9 +1,90 @@
 @extends('report::layouts.master')
 
 @section('content')
-    <h1>Hello World</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-    <p>
-        This view is loaded from module: {!! config('report.name') !!}
-    </p>
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <center>
+                            {{--<div class="col-sm-12">
+                                <div class="panel panel-primary">
+                                    <div class="panel-body">
+                                        {{ $addStockChart->container() }}
+                                    </div>
+                                </div>
+                            </div>--}}
+                            <div class="col-sm-12">
+                                <div class="panel panel-primary">
+                                    <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
+                                        PENAMBAHAN STOCK HARI INI
+                                    </div>
+                                    <div class="panel-body" style="color: #8c8c8c">
+                                        <table class="table">
+                                            <thead>
+                                            <tr>
+                                                <th>Nama Barang</th>
+                                                <th>Merek Barang</th>
+                                                <th>Jumlah</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach($todayProductStocks as $todayProductStock)
+                                                    <tr>
+                                                        <td>{{ $todayProductStock->product->name }}</td>
+                                                        <td>{{ $todayProductStock->product->productBrand->name }}</td>
+                                                        <td>{{ $todayProductStock->quantity }}</td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="panel panel-success">
+                                    <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
+                                        PENAMBAHAN STOCK BULAN INI
+                                    </div>
+                                    <div class="panel-body" style="color: #8c8c8c">
+                                        <table class="table">
+                                            <thead>
+                                            <tr>
+                                                <th>Nama Barang</th>
+                                                <th>Merek Barang</th>
+                                                <th>Tanggal</th>
+                                                <th>Jumlah</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach($thisMonthProductStocks as $thisMonthProductStock)
+                                                <tr>
+                                                    <td>{{ $thisMonthProductStock->product->name }}</td>
+                                                    <td>{{ $thisMonthProductStock->product->productBrand->name }}</td>
+                                                    <td>{{ $thisMonthProductStock->date }}</td>
+                                                    <td>{{ $thisMonthProductStock->quantity }}</td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
+{{--
+@section('script')
+    {{ $addStockChart->script() }}
+@endsection--}}
