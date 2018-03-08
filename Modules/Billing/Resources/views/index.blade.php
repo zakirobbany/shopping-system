4b4469e514fb10f34e4e7d2b8fc1cdded9379c7f
zaki <zaki.robbany22@gmail.com>
2018-03-08 16:09:04 +0700

    Update billing lobby

--- Modules/Billing/Resources/views/index.blade.php
+++ Modules/Billing/Resources/views/index.blade.php
@@ -1,9 +1,37 @@
 @extends('billing::layouts.master')
 
 @section('content')
-    <h1>Hello World</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-    <p>
-        This view is loaded from module: {!! config('billing.name') !!}
-    </p>
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <center>
+                            <div class="col-sm-12">
+                                <div class="panel panel-primary">
+                                    <div class="panel-body">
+                                        Penjualan Hari Ini
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="panel panel-primary">
+                                    <div class="panel-body">
+                                        Penjualan Bulan Ini
+                                    </div>
+                                </div>
+                            </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
