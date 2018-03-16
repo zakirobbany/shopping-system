bcca376d274780b05ed284a1cc0b7fc6e3c76b70
zaki <zaki.robbany22@gmail.com>
2018-03-16 11:34:57 +0700

    added billing model

---
+++ app/Models/Billing/Payment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\Billing;
+
+use App\Models\Core\Customer;
+use App\Models\Inventory\Product;
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Payment extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'date',
+        'quantity',
+        'total_price',
+        'discount',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
