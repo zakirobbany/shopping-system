c84a85ebf82b3f05e3539b051041a473d6faa6a1
zaki <zaki.robbany22@gmail.com>
2018-03-16 11:35:26 +0700

    added payment Migration

---
+++ database/migrations/2018_03_16_033455_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('customer_id')->nullable();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->date('date');
+            $table->integer('quantity')->nullable();
+            $table->bigInteger('discount')->nullable();
+            $table->bigInteger('total_price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
