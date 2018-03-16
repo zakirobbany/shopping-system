2f51b62cd87d202ddf92ac1b87560a1a3d21d3dc
zaki <zaki.robbany22@gmail.com>
2018-03-16 11:36:44 +0700

    added payment route

--- Modules/Billing/Http/routes.php
+++ Modules/Billing/Http/routes.php
@@ -3,4 +3,6 @@
 Route::group(['middleware' => 'web', 'prefix' => 'billing', 'namespace' => 'Modules\Billing\Http\Controllers'], function()
 {
     Route::get('/', 'BillingController@index');
+
+    Route::resource('payment', 'PaymentController');
 });
