f4ce27d12f0df4f7687e58da8cfd1711cc19a7f6
zaki <zaki.robbany22@gmail.com>
2018-03-19 13:51:33 +0700

    Update modern payment vue component

---
+++ resources/assets/js/components/ModernPayment.vue
@@ -0,0 +1,39 @@
+<template>
+    <div :class="['form-group']">
+        <label for="product" class="control-label col-sm-2">barang</label>
+        <div class="col-sm-4">
+            <select v-model="product.name" id="product" class="form-control">
+
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                products: [],
+                selectedProduct: '',
+            }
+        },
+
+        mounted: function () {
+            this.getProducts()
+        },
+
+        methods: {
+            getProduct: function () {
+                axios.get('/api/products')
+                    .then(response => {
+                        let products = response.data.data
+                        this.products = products
+                    })
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
