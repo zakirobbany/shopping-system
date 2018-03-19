d89218b40d554f64b793abb0d6430ccfc0df6af0
zaki <zaki.robbany22@gmail.com>
2018-03-19 13:53:38 +0700

    retrieve monthly billing data

--- Modules/Billing/Resources/views/index.blade.php
+++ Modules/Billing/Resources/views/index.blade.php
@@ -19,11 +19,23 @@
                                 <div class="panel panel-primary">
                                     <div class="panel-body">
                                         <div class="pull-left">Penjualan Hari Ini</div>
-                                        <div class="pull-right">{{ $countBilling }}</div>
+                                        <div class="pull-right">{{ $dailyCountBilling }}</div>
                                     </div>
                                     <div class="panel-body">
                                         <div class="pull-left">Penghasilan Hari Ini</div>
-                                        <div class="pull-right">Rp. {{ number_format($todayBilling) }}</div>
+                                        <div class="pull-right">Rp. {{ number_format($dailyBilling) }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="panel panel-danger">
+                                    <div class="panel-body">
+                                        <div class="pull-left"><P></P>enjualan Bulan Ini</div>
+                                        <div class="pull-right">{{ $monthlyCountBilling }}</div>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="pull-left">Penghasilan Bulan Ini</div>
+                                        <div class="pull-right">Rp. {{ number_format($monthlyBilling) }}</div>
                                     </div>
                                 </div>
                             </div>
