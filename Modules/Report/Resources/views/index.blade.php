b5edec1df428e23cf9bdfc00b1a6485bdebaef57
zaki <zaki.robbany22@gmail.com>
2018-03-24 15:29:35 +0700

    update : - added daily profit report - added monthly profit report

--- Modules/Report/Resources/views/index.blade.php
+++ Modules/Report/Resources/views/index.blade.php
@@ -1,93 +1,276 @@
 @extends('report::layouts.master')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-md-offset-0">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-md-offset-0">
+        <div class="panel panel-default">
+          <div class="panel-heading">Dashboard</div>
 
-                    <div class="panel-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <center>
-                            <div class="col-sm-12">
-                                <div class="panel panel-primary">
-                                    <div class="panel-body">
-                                        {!! $addStockChart->container() !!}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-12">
-                                <div class="panel panel-primary">
-                                    <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
-                                        PENAMBAHAN STOCK HARI INI
-                                        <span>
-                                            {{ trans('messages.' . $now->format('l')) . ' ' .
-                                            $now->toDateString() }}
-                                        </span>
-                                    </div>
-                                    <div class="panel-body" style="color: #8c8c8c">
-                                        <table class="table">
-                                            <thead>
-                                            <tr>
-                                                <th>Nama Barang</th>
-                                                <th>Merek Barang</th>
-                                                <th>Jumlah</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                                @foreach($todayProductStocks as $todayProductStock)
-                                                    <tr>
-                                                        <td>{{ $todayProductStock->product->name }}</td>
-                                                        <td>{{ $todayProductStock->product->productBrand->name }}</td>
-                                                        <td>{{ $todayProductStock->quantity }}</td>
-                                                    </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-12">
-                                <div class="panel panel-success">
-                                    <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
-                                        PENAMBAHAN STOCK BULAN INI
-                                    </div>
-                                    <div class="panel-body" style="color: #8c8c8c">
-                                        <table class="table">
-                                            <thead>
-                                            <tr>
-                                                <th>Nama Barang</th>
-                                                <th>Merek Barang</th>
-                                                <th>Tanggal</th>
-                                                <th>Jumlah</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            @foreach($thisMonthProductStocks as $thisMonthProductStock)
-                                                <tr>
-                                                    <td>{{ $thisMonthProductStock->product->name }}</td>
-                                                    <td>{{ $thisMonthProductStock->product->productBrand->name }}</td>
-                                                    <td>{{ $thisMonthProductStock->date }}</td>
-                                                    <td>{{ $thisMonthProductStock->quantity }}</td>
-                                                </tr>
-                                            @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </center>
-                    </div>
+          <div class="panel-body">
+            @if (session('status'))
+              <div class="alert alert-success">
+                {{ session('status') }}
+              </div>
+            @endif
+            <center>
+              <div class="col-sm-12">
+                <div class="panel panel-primary">
+                  <div class="panel-body">
+                    {!! $addStockChart->container() !!}
+                  </div>
                 </div>
-            </div>
+              </div>
+
+              <div class="col-sm-12">
+                <div class="panel panel-primary">
+                  <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
+                    PENAMBAHAN STOCK HARI INI
+                    <span>
+                      {{ trans('messages.' . $now->format('l')) . ' ' . $now->toDateString() }}
+                    </span>
+                  </div>
+                  <div class="panel-body" style="color: #8c8c8c">
+                    <table class="table">
+                      <thead>
+                      <tr>
+                        <th>Nama Barang</th>
+                        <th>Merek Barang</th>
+                        <th>Jumlah</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      @foreach($todayProductStocks as $todayProductStock)
+                        <tr>
+                          <td>{{ $todayProductStock->product->name }}</td>
+                          <td>{{ $todayProductStock->product->productBrand->name }}</td>
+                          <td>{{ $todayProductStock->quantity }}</td>
+                        </tr>
+                      @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-12">
+                <div class="panel panel-success">
+                  <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
+                    PENAMBAHAN STOCK BULAN INI
+                  </div>
+                  <div class="panel-body" style="color: #8c8c8c">
+                    <table class="table">
+                      <thead>
+                      <tr>
+                        <th>Nama Barang</th>
+                        <th>Merek Barang</th>
+                        <th>Tanggal</th>
+                        <th>Jumlah</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      @foreach($thisMonthProductStocks as $thisMonthProductStock)
+                        <tr>
+                          <td>{{ $thisMonthProductStock->product->name }}</td>
+                          <td>{{ $thisMonthProductStock->product->productBrand->name }}</td>
+                          <td>{{ $thisMonthProductStock->date }}</td>
+                          <td>{{ $thisMonthProductStock->quantity }}</td>
+                        </tr>
+                      @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-12">
+                <div class="panel panel-primary">
+                  <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
+                    PENJUALAN HARI INI
+                    <span>
+                      {{ trans('messages.' . $now->format('l')) . ' ' . $now->toDateString() }}
+                    </span>
+                  </div>
+                  <div class="panel-body" style="color: #8c8c8c">
+                    <table class="table">
+                      <thead>
+                      <tr>
+                        <th>Pembeli</th>
+                        <th>Nama Barang</th>
+                        <th>Merek Barang</th>
+                        <th>Harga</th>
+                        <th>Jumlah</th>
+                        <th>Potongan</th>
+                        <th>Jumlah Harga</th>
+                        <th>Total Pembayaran</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      @foreach($todayPayments as $todayPayment)
+                        <tr>
+                          <td>
+                            {{ $todayPayment->customer->name }}
+                          </td>
+                          <td>
+                            @foreach($todayPayment->products as $product)
+                              {{ $product->name }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($todayPayment->products as $product)
+                              {{ $product->productBrand->name }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($todayPayment->products as $product)
+                              Rp. {{ number_format($product->sell_price) }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($todayPayment->products as $product)
+                              {{ $product->pivot->quantity }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($todayPayment->products as $product)
+                              Rp. {{ number_format($product->pivot->discount) }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($todayPayment->products as $product)
+                              Rp. {{ number_format($product->pivot->total_price) }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            Rp. {{ number_format($todayPayment->total_payment) }}
+                          </td>
+                        </tr>
+                      @endforeach
+                      <tr>
+                        <td><b>Total Pendapatan</b></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><b>Rp. {{ number_format($todayPayments->sum('total_payment')) }}</b></td>
+                      </tr>
+                      <tr>
+                        <td><b>Total Profit</b></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><b>Rp. {{ number_format($todayProfit) }}</b></td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-12">
+                <div class="panel panel-success">
+                  <div class="panel-title" style="margin-top: 10px; color: #8c8c8c">
+                    PENJUALAN BULAN INI
+                  </div>
+                  <div class="panel-body" style="color: #8c8c8c">
+                    <table class="table">
+                      <thead>
+                      <tr>
+                        <th>Tanggal</th>
+                        <th>Pembeli</th>
+                        <th>Nama Barang</th>
+                        <th>Merek Barang</th>
+                        <th>Harga</th>
+                        <th>Jumlah</th>
+                        <th>Potongan</th>
+                        <th>Jumlah Harga</th>
+                        <th>Total Pembayaran</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      @foreach($thisMonthPayments as $thisMonthPayment)
+                        <tr>
+                          <td>
+                            {{ $thisMonthPayment->date }}
+                          </td>
+                          <td>
+                            {{ $thisMonthPayment->customer->name }}
+                          </td>
+                          <td>
+                            @foreach($thisMonthPayment->products as $product)
+                              {{ $product->name }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($thisMonthPayment->products as $product)
+                              {{ $product->productBrand->name }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($thisMonthPayment->products as $product)
+                              Rp. {{ number_format($product->sell_price) }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($thisMonthPayment->products as $product)
+                              {{ $product->pivot->quantity }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($thisMonthPayment->products as $product)
+                              Rp. {{ number_format($product->pivot->discount) }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            @foreach($thisMonthPayment->products as $product)
+                              Rp. {{ number_format($product->pivot->total_price) }} <br/>
+                            @endforeach
+                          </td>
+                          <td>
+                            Rp. {{ number_format($thisMonthPayment->total_payment) }}
+                          </td>
+                        </tr>
+                      @endforeach
+                      <tr>
+                        <td><b>Total Pendapatan</b></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><b>Rp. {{ number_format($thisMonthPayments->sum('total_payment')) }}</b></td>
+                      </tr>
+                      <tr>
+                        <td><b>Total Profit</b></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><b>Rp. {{ number_format($thisMonthProfit) }}</b></td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+            </center>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 @endsection
 @push('script')
-    {!! $addStockChart->script()  !!}
+  {!! $addStockChart->script()  !!}
 @endpush
