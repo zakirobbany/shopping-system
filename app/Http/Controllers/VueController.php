d97ed11ed4d65e311bb23d98d6388361e3a97190
zaki <zaki.robbany22@gmail.com>
2018-03-24 13:12:25 +0700

    added get customers method

--- app/Http/Controllers/VueController.php
+++ app/Http/Controllers/VueController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Core\Customer;
 use App\Models\Inventory\Product;
 use Illuminate\Http\Request;
 
 class VueController extends Controller
 {
+    public function getCustomers()
+    {
+        return [
+            'data' => Customer::orderBy('name', 'asc')->get(),
+        ];
+    }
+
     public function getProducts()
     {
         return [
