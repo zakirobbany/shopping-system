a2c88f3de0607a9b4762b6a6c11fe674ed0649fa
zaki <zaki.robbany22@gmail.com>
2018-03-31 14:17:42 +0700

    added route for stock and sell page

--- Modules/Report/Http/routes.php
+++ Modules/Report/Http/routes.php
@@ -3,4 +3,6 @@
 Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'report', 'namespace' => 'Modules\Report\Http\Controllers'], function()
 {
     Route::get('/', 'ReportController@index');
+    Route::get('/stock', 'ReportController@stock')->name('report.stock');
+    Route::get('/sell', 'ReportController@sell')->name('report.sell');
 });
